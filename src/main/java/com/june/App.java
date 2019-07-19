9d293dcaa11e676e2f4bdd2fa0566a6e491b1075
rajesh1274 <51088515+rajesh1274@users.noreply.github.com>
2019-07-19 10:33:59 +0530

    Update App.java

--- src/main/java/com/june/App.java
+++ src/main/java/com/june/App.java
@@ -7,7 +7,7 @@ package com.june;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+String name = "Rajesh";
 @SpringBootApplication
 public class App 
 {
